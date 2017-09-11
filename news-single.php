3493bf63700fa6127c2c2da96f7cb466c00091d7
Davey <daveymoores@gmail.com>
2017-09-11 15:56:20 +0100

    fonts now loading correctly - partial news content page

---
+++ news-single.php
@@ -0,0 +1,85 @@
+<?php include 'components/head.php'; ?>
+
+<?php include 'components/navigation.php'; ?>
+
+<section class="hero--news-story">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="news-story--content">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-offset-1 col-md-10 col-sm-offset-1 col-sm-10">
+				<p><strong>The following article is for professional advisers only and is not intended to be shown to retail investors.</strong></p>
+
+				<p>“Never put all your eggs in one basket” is sound advice in the investment field as it is in any other. Diversification of an investment portfolio, of course, means holding a variety of different asset classes, and across different sub-sectors and geographic locations.</p>
+
+				<p>Diversification is important as different types of assets behave in different ways depending on the economic factors driving them. Sentiment of investors can also play a significant part in short-term performance of different assets. As values of different asset classes tend not to head in the same direction at the same time, diversification can enhance the volatility/return profile of an investment portfolio. The chart below shows how different fund sectors have performed by looking back at the annual return every month for the past ten years.</p>
+
+				<p>As you would expect, of the ‘traditional’ asset classes, equities tend to be the most volatile but offer the potential for high returns. Cash is the most stable asset class but offers little prospect for investment return, often providing negative returns in ‘real’ terms after allowing for the effects of inflation. Others such as fixed income and commercial property tend to sit somewhere between them.</p>
+
+				<img src="./dist/images/stock/stock-bg-1.png" alt="" />
+
+				<h4>Portfolio construction</h4>
+
+				<p>It has always been common practice to hold a blend of these traditional asset classes in a portfolio, weighting more into equities for the more adventurous investors or those with long-term investment horizons. Conversely, those with lower appetites for risk or with short-term investment needs, usually weight their portfolios more heavily to cash and fixed income.</p>
+
+				<p>However, one asset class which has previously been missing from investment portfolios is residential property – despite its track record of providing strong yet relatively stable investment returns, and the fact it has little correlation with the traditional asset classes. Despite a large number of private landlords investing directly into buy-to-let properties, the options have been very limited for those who want a more diverse portfolio or hold their investments on fund platforms and in ISAs or SIPPS. The next chart shows the frequency and range of distributions for the same IA sectors as the chart above, but also includes the well-known Halifax House Price Index to represent residential property as an asset class.</p>
+
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="news-story--author">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-offset-1 col-md-10 col-sm-offset-1 col-sm-10">
+				<h2>About the author</h2>
+
+				<div class="news-story--author--content">
+					<div class="row">
+						<div class="col-md-3">
+							<div class="news-story--img-cont">
+								<img src="./dist/images/profile-pics/quote-placeholder.jpg" alt="">
+							</div>
+						</div>
+						<div class="col-md-offset-1 col-md-8">
+							<h3>About the author</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php include 'components/guide-sign-up.php'; ?>
+
+<section class="news-story--navigation">
+	<a href="#_" class="column eq" style="background-image: url(./dist/images/stock/stock-bg-1.png);">
+		<div class="news-story--bg"></div>
+		<div class="news-story--item">
+			<h2>ISAs: still the best investment after 20 years</h2>
+			<p>Previous Post</p>
+		</div>
+	</a>
+	<a href="#_" class="column eq" style="background-image: url(./dist/images/stock/stock-bg-2.png);">
+		<div class="news-story--bg"></div>
+		<div class="news-story--item">
+			<h2>ISAs: still the best investment after 20 years</h2>
+			<p>Next Post</p>
+		</div>
+	</a>
+</section>
+
+<?php include 'components/footer.php'; ?>
+
+<?php include 'components/foot.php'; ?>
