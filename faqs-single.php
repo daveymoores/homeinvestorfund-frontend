fc55566cf753c189b50fcf8ca440b252408e4311
Davey <daveymoores@gmail.com>
2018-01-08 19:24:44 +0000

    faqs single

---
+++ faqs-single.php
@@ -0,0 +1,74 @@
+<?php include 'components/head.php'; ?>
+<?php include 'components/navigation.php'; ?>
+
+<section class="hero--faqs">
+	<div class="centered-heading">
+		<heading>
+			<h2>FAQs</h2>
+		</heading>
+	</div>
+</section>
+
+<div class="faqs--page-wrapper">
+	<section class="faqs faqs__breadcrumb">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<ul class="faqs__breadcrumb--list">
+						<li class="faqs__breadcrumb--item">FAQs</li>
+						<li class="faqs__breadcrumb--item">Getting started</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="faqs">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+
+					<div class="faqs__q-list">
+						<h3>Getting started</h3>
+
+						<ul class="faqs__q-list--ul">
+							<li class="faqs__q-list--item"><a data-target href="">What is homeinvestor.fund?</a></li>
+							<li class="faqs__q-list--item"><a data-target href="">Who is Wealth Kernel</a></li>
+							<li class="faqs__q-list--item"><a data-target href="">Who is Thesis?</a></li>
+							<li class="faqs__q-list--item"><a data-target href="">Lorem ipsum?</a></li>
+							<li class="faqs__q-list--item"><a data-target href="">Lorem ipsum?</a></li>
+							<li class="faqs__q-list--item"><a data-target href="">Lorem ipsum?</a></li>
+							<li class="faqs__q-list--item"><a data-target href="">Lorem ipsum?</a></li>
+							<li class="faqs__q-list--item"><a data-target href="">Lorem ipsum?</a></li>
+							<li class="faqs__q-list--item"><a data-target href="">Lorem ipsum?</a></li>
+						</ul>
+					</div>
+
+				</div>
+				<div class="col-md-6"></div>
+			</div>
+
+		</div>
+	</section>
+</div>
+
+<div class="map__overlay map__overlay--home" data-widget="info-overlay">
+	<a href="#" class="map__overlay--close"><span>+</span></a>
+	<div class="map__overlay--title">
+		<h4>How were these figures calculated?</h4>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+</div>
+<div class="map__overlay--bg" id="overlayBackgroundHook"></div>
+
+<div class="nav-options--border">
+	<?php include 'components/nav-options.php'; ?>
+</div>
+
+<?php include 'components/guide-sign-up.php'; ?>
+
+<?php include 'components/call-out.php'; ?>
+
+<?php include 'components/footer.php'; ?>
+
+<?php include 'components/foot.php'; ?>
