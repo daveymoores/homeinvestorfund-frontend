0d2652b03f69db9367c2cba249db41ebc0630e72
Davey <daveymoores@gmail.com>
2018-01-11 13:45:55 +0000

    adding functionality for less than 3 slides on homepage

--- assets/scripts/widgets/carousel.js
+++ assets/scripts/widgets/carousel.js
@@ -10,13 +10,13 @@ Carousel.prototype.init = function(){
     this.ww = window.innerWidth;
     this.slider = this.node;
     this.parent = this.slider.parentNode;
-    this.parent.classList.add('swiper-initialised');
     this.slides = this.node.querySelectorAll('.swiper-slide');
 
     this.swiper = new Swiper(this.slider, {
       slidesPerView: 3,
       spaceBetween: 30,
       loop: true,
+      init: false,
       pagination: {
          el: '.swiper-pagination',
       },
@@ -47,20 +47,31 @@ Carousel.prototype.init = function(){
 Carousel.prototype.buildCarousel = function(ww){
     ww = typeof ww !== 'undefined' ? ww : this.ww;
 
+    var carouselSwiper = this.node.swiper;
+
     var ll = this.slides.length;
     console.log(ll);
 
     if(ww < 998) {
         if(ll > 2) {
             this.swiper.init();
+            this.parent.classList.add('swiper-initialised');
+        } else {
+            carouselSwiper.destroy();
         }
     } else if(ww < 500) {
         if(ll > 1) {
             this.swiper.init();
+            this.parent.classList.add('swiper-initialised');
+        } else {
+            carouselSwiper.destroy();
         }
     } else {
         if(ll > 3) {
             this.swiper.init();
+            this.parent.classList.add('swiper-initialised');
+        } else {
+            carouselSwiper.destroy();
         }
     }
 
