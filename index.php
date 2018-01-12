56b74489e6e865017d2b08d97e4d51c4ebd674ab
Davey <daveymoores@gmail.com>
2018-01-12 12:33:13 +0000

    removing overlay from all screens

--- index.php
+++ index.php
@@ -26,15 +26,6 @@
 			</div>
 		</div>
 	</div>
-
-	<div class="map__overlay map__overlay--home" data-widget="info-overlay">
-		<a href="#" class="map__overlay--close"><span>+</span></a>
-		<div class="map__overlay--title">
-			<h4>How were these figures calculated?</h4>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-		</div>
-	</div>
-	<div class="map__overlay--bg" id="overlayBackgroundHook"></div>
 </section>
 
 <div class="calculator--outer-wrapper">
