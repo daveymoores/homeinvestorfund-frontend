3a742c150f010a5cb88cc691adc6c0c137b70fbb
Davey <daveymoores@gmail.com>
2018-01-12 13:14:22 +0000

    wrong svg vendor file

--- components/foot.php
+++ components/foot.php
@@ -1,4 +1,4 @@
-<script type="text/javascript" src="./svg-polyfill/svg4everybody.js"></script>
+<script type="text/javascript" src="./svg-polyfill/svg4everybody.min.js"></script>
 <script>svg4everybody();</script>
 <script type="text/javascript" src="./dist/scripts/main.min.js"></script>
 </body>
