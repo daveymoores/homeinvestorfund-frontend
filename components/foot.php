cbf254a19ddb7dea21dda4c1c8db68c54589d07f
Davey <daveymoores@gmail.com>
2018-01-12 13:11:24 +0000

    forgot to run svgfor everybody

--- components/foot.php
+++ components/foot.php
@@ -1,4 +1,5 @@
 <script type="text/javascript" src="./svg-polyfill/svg4everybody.js"></script>
+<script>svg4everybody();</script>
 <script type="text/javascript" src="./dist/scripts/main.min.js"></script>
 </body>
 </html>
